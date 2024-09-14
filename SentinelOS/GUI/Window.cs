204b8713ed3d550d20578148ed33980f43d2a596
mathlokz <matheusdepmendonca@gmail.com>
2024-09-13 21:08:56 -0300

    feat: abstract class Window for handling apps & WindowManager for managing apps interface

---
+++ SentinelOS/GUI/Window.cs
@@ -0,0 +1,37 @@
+﻿using Cosmos.System.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentinelOS.GUI
+{
+    public abstract class Window
+    {
+        protected Canvas canvas;
+        protected int windowX;
+        protected int windowY;
+        protected int windowWidth;
+        protected int windowHeight;
+        protected bool isRunning;
+
+        public Window(Canvas canvas, int x, int y, int width, int height)
+        {
+            this.canvas = canvas;
+            this.windowX = x;
+            this.windowY = y;
+            this.windowWidth = width;
+            this.windowHeight = height;
+            this.isRunning = false;
+        }
+
+        public abstract void Initialize();
+        public abstract void Initialize(string path);
+        public abstract void HandleKeyPress(ConsoleKeyInfo keyInfo);
+        public abstract void Draw();
+
+        public bool IsRunning => isRunning;
+
+    }
+}
---
+++ SentinelOS/GUI/WindowManager.cs
@@ -0,0 +1,42 @@
+﻿using Cosmos.System.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentinelOS.GUI
+{
+    public class WindowManager
+    {
+        private Canvas canvas;
+        private List<Window> windows;
+
+        public WindowManager(Canvas canvas)
+        {
+            this.canvas = canvas;
+            this.windows = new List<Window>();
+        }
+
+        public void AddWindow(Window window)
+        {
+            windows.Add(window);
+        }
+
+        public void Run()
+        {
+            foreach (var window in windows)
+            {
+                if (window.IsRunning)
+                {
+                    window.Draw();
+                    if (Console.KeyAvailable)
+                    {
+                        var keyInfo = Console.ReadKey(true);
+                        window.HandleKeyPress(keyInfo);
+                    }
+                }
+            }
+        }
+    }
+}
