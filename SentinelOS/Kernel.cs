0f609792c478b1dc2013e256c766624c28fcf2d8
mathlokz <matheusdepmendonca@gmail.com>
2024-09-17 18:20:35 -0300

    feat: IManager and clean-up kernel code

--- SentinelOS/Kernel.cs
+++ SentinelOS/Kernel.cs
@@ -1,62 +1,36 @@
 ﻿using Cosmos.Core.Memory;
-using Cosmos.System.Graphics;
-using IL2CPU.API.Attribs;
-using SentinelOS.GUI;
 using SentinelOS.Resources;
 using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Runtime.CompilerServices;
-using System.Text;
 using Sys = Cosmos.System;
-using MouseManager = Cosmos.System.MouseManager;
-using MouseState = Cosmos.System.MouseState;
-using Cosmos.System.Graphics.Fonts;
-using System.IO;
 using Cosmos.System.FileSystem.VFS;
-using Cosmos.System.FileSystem.Listing;
+using Cosmos.System.FileSystem;
 
 namespace SentinelOS
 {
     public class Kernel : Sys.Kernel
     {
         private ConsoleManager consoleManager;
-        private Canvas canvas;
-        private UserInterface userInterface;
+        private InterfaceManager interfaceManager;
 
         protected override void BeforeRun()
         {
-            Console.Clear();
             Console.WriteLine("SentinelOS is booting...");
-
             DirectoryManager.CreateSystemFiles();
-            DirectoryManager.CurrentPath = Config.Desktop;
-            DirectoryManager.ClearDir();
+            DirectoryManager.ClearDir(true);
             consoleManager = new ConsoleManager();
 
-            MouseManager.ScreenWidth = 1280;
-            MouseManager.ScreenHeight = 720;
-            canvas = FullScreenCanvas.GetFullScreenCanvas(new Mode(1280, 720, ColorDepth.ColorDepth32));
-            MouseManager.X = 1280 / 2;
-            MouseManager.Y = 720 / 2;
-
-            userInterface = new UserInterface(canvas);
-
-            // Add tasks to the task manager
-            // Example with a method that have arguments
-            // taskManager.AddTask("ExampleTaskWithParameters", 0, new Action<int, string>(ExampleTaskWithParameters), 42, "Hello");
-            TaskManager.AddTask("DrawUserInterface", 0, new Action(userInterface.DrawUserInterface));
-            TaskManager.AddTask("HandleMouseInput", 0, new Action(userInterface.HandleMouseInput));
-
             Console.WriteLine("SentinelOS has booted successfully!");
-            Console.WriteLine("Type 'help' for a list of commands.");
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+
+            interfaceManager = new InterfaceManager(1280, 720);
+            TaskManager.AddTask("InterfaceManager", 0, new Action(interfaceManager.Run));
         }
 
         protected override void Run()
         {
             TaskManager.ExecuteAllTasks();
             Heap.Collect();
-            canvas.Display();
         }
     }
 }
---
+++ SentinelOS/Resources/InterfaceManager.cs
@@ -0,0 +1,41 @@
+﻿using Cosmos.System.Graphics;
+using SentinelOS.GUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using MouseManager = Cosmos.System.MouseManager;
+
+namespace SentinelOS.Resources
+{
+    class InterfaceManager
+    {
+        private Canvas canvas;
+        private UserInterface userInterface;
+
+        public InterfaceManager(int width, int height)
+        {
+            canvas = FullScreenCanvas.GetFullScreenCanvas(new Mode(width, height, ColorDepth.ColorDepth32));
+            SetupMouse(width, height);
+            userInterface = new UserInterface(canvas);
+        }
+
+        private void SetupMouse(int width, int height)
+        {
+            MouseManager.ScreenWidth = (uint)width;
+            MouseManager.ScreenHeight = (uint)height;
+            MouseManager.X = (uint)width / 2;
+            MouseManager.Y = (uint)height / 2;
+        }
+
+
+        public void Run()
+        {
+            userInterface.Run();
+            canvas.Display();
+        }
+
+    }
+}
